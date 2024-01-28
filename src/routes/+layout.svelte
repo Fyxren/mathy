0699e7ae91448fc0ed5b2be76815c98032104ab0
Fyxren <benweerd@gmail.com>
2024-01-28 13:36:18 +0100

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Topbar from '$lib/components/elements/Topbar.svelte';
+	import '../app.css';
+</script>
+
+<div class="container h-screen max-h-screen px-16 space-y-4">
+	<Topbar />
+	<div class="">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container flex flex-col items-center justify-center py-40">
+	<h1 class="font-mono text-6xl font-bold">Mathy</h1>
+	<p class="italic">Improve your mental math. No trackers, no cookies, no nonsense.</p>
+	<div class="flex items-center justify-center gap-4 py-8">
+		<a href="/play" class="btn btn-primary btn-wide">Play</a>
+		<a href="/history" class="btn btn-primary btn-wide btn-outline">History</a>
+	</div>
+</div>
