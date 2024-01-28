a8ea94140299fd9222a995470e960b0d46549368
Fyxren <benweerd@gmail.com>
2024-01-28 22:05:52 +0100

    refactor: update topbar text

--- src/lib/components/elements/Topbar.svelte
+++ src/lib/components/elements/Topbar.svelte
@@ -1,6 +1,6 @@
 <div class="h-16 border-b">
 	<div class="flex items-center justify-between h-full">
-		<div class="flex items-center">00:00:00</div>
-		<div class="flex items-center">0/0</div>
+		<div class="flex items-center">Mathy</div>
+		<div class="flex items-center">ᓚᘏᗢ</div>
 	</div>
 </div>
